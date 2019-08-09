8b53730366bf1b584a5f11bc817f8ed7688cc529
Gary Storey <gary@digitalcrafts.com>
2019-08-09 08:13:43 -0400

    comments added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-
+// Include the bluebird promise library
+// This might not be necesary anymore!!
 const promise = require('bluebird');
 
+// initalization options to pass to the pg-promise library
 const initOptions = {
     promiseLib: promise
 }
-
+// Our database information
+// since Port is the standard port this is optional
+// same for the user if it is the default user
 const config = {
     host: 'localhost',
     post: 5432,
@@ -12,10 +16,13 @@ const config = {
     user: 'digitalcrafts'
 };
 
+// Include the module in our code
 const pgg = require('pg-promise');
+//execute the returned function, pasdsing our initialization 
 const pg = pgg(initOptions);
+// Now connect to the the database
 const db = pg(config);
-
+//use the db object to query our database
 db.query(`select * from album`).then( results => {
     console.log(results);
 });
